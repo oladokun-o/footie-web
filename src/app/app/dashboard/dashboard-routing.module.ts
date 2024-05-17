02b554c7ed254e54341f646d9348e7ed3f351ad0
Oladokun Oladipupo <115462144+oladokuno@users.noreply.github.com>
2024-05-17 12:26:37 +0100

    feat: Add new dashboard pages and modules

--- src/app/app/dashboard/dashboard-routing.module.ts
+++ src/app/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'orders',
+        loadChildren: () => import('./pages/orders/orders.module').then(m => m.OrdersModule)
+      },
+      {
+        path: 'notifications',
+        loadChildren: () => import('./pages/notifications/notifications.module').then(m => m.NotificationsModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule)
+      }
+    ]
   }
 ];
 
